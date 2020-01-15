0ddc23214b420e7009ace4b56aa52aaaa8f7ddc2
Jhon Peñalva Sanchez <54199064+jons255-p@users.noreply.github.com>
2020-01-15 18:15:28 -0500

    Add files via upload
    CLASE 2 python-uni

---
+++ jon.py
@@ -0,0 +1,38 @@
+
+#x=10
+#y=10
+#pharse ="hello world"
+#print(x)
+#print(y)
+#print(pharse)
+#print(type(x))
+#print(type(y))
+#multiplicacion
+x=3*5
+
+print("el resultado de 3*5 es",x)
+
+x=3/5 # division flotante
+print(f"el rstado de 3/5 es: {x}")
+x=6//5  # division entera(sale parte entera
+print("el resultado de 6//5 entero es",x)
+
+x=10%7  # el resto de la division
+print("el resultado de 10%7 divisiendo y bteniendo el resto",x)
+
+###OPERACION POTENCIA##
+x=10**3
+print("la potencia de 10 al 3",x)
+
+variable=input("ingrese un entero =")
+print(variable)
+print(type(variable))
+
+variable=int(input("ingrese un entero =") ) #refundicion
+print(variable)
+print(type(variable))
+
+#calculo del area de un circulo
+r=input("ingrese el radio del circulo")
+area=3.14*(r**2)
+print("el area del circulo es =",area)
---
+++ jon1.py
@@ -0,0 +1,5 @@
+#calculo del area de un circulo
+import math
+r=float(input("ingrese el radio del circulo ="))  #
+area=round(math.pi*r**2,2)
+print("el área del circulo es =",area)
